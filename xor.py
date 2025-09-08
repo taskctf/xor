ef90968d65ba834ea73cb8e521d9045de8d42716
taskctf <taskctf@gmail.com>
2025-09-08 18:18:37 +0500

    Create xor.py

---
+++ xor.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import numpy as np
+
+A = np.array(Image.open("stego.png").convert("RGB"))
+B = np.array(Image.open("twin.png").convert("RGB"))
+
+xor0 = (A[:,:,0] ^ B[:,:,0])
+xor1 = (A[:,:,1] ^ B[:,:,1])
+xor2 = (A[:,:,2] ^ B[:,:,2])
+combined = (( (xor0 | xor1 | xor2) & 1 ) * 255).astype('uint8')  # if BIT=0
+Image.fromarray(combined, 'L').save("secret_key.png")
