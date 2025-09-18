de988a2c9903c616de9e231d86b88201a1fb56ea
taskctf <taskctf@gmail.com>
2025-09-18 19:28:30 +0500

    Update xor.py

--- xor.py
+++ xor.py
@@ -7,5 +7,5 @@ B = np.array(Image.open("twin.png").convert("RGB"))
 xor0 = (A[:,:,0] ^ B[:,:,0])
 xor1 = (A[:,:,1] ^ B[:,:,1])
 xor2 = (A[:,:,2] ^ B[:,:,2])
-combined = (( (xor0 | xor1 | xor2) & 1 ) * 255).astype('uint8')  # if BIT=0
+combined = (( (xor0 | xor1 | xor2) & 1 ) * 255).astype('uint8') 
 Image.fromarray(combined, 'L').save("secret_key.png")
